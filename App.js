20bfccec72dcdd1d0a32ccd517082aec3e4d31fe
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2021-04-21 20:56:35 -0600

    commit 3 - 75%

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState }from 'react';
+import { TextInput, View, Text, Switch, AppState} from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen'
 import TestStyles from './app/screens/TestStyles'
@@ -12,9 +12,30 @@ import Screen from './app/components/Screen'
 import Icon from './app/components/Icon'
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
+import AppTextInput from './app/components/AppTextInput';
+import AppPicker from './app/components/AppPicker';
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 }
+];
 
 export default function App() {
-  return <ListingsScreen />
+
+  const [category, setCategory] = useState(categories[0]);
+
+  return (
+    <Screen>
+      <AppPicker 
+        selectedItem={category}
+        onSelectItem={item => setCategory(item)}
+        items={categories} 
+        placeholder="Category" 
+        icon='apps' />
+      <AppTextInput placeholder='Email' icon='email' />
+    </Screen>
+  );
     
 }
 
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,17 +1,12 @@
 import React from 'react'
-import { Text, StyleSheet } from 'react-native'
+import { Text } from 'react-native'
+
+import defaultStyles from '../config/styles'
 
 export default function AppText(props) {
     return (
-        <Text style={[styles.text, props.style]}>
+        <Text style={[defaultStyles.text, props.style]}>
             {props.children}
         </Text>
     )
 }
-
-const styles = StyleSheet.create({
-    text: {
-        fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-        fontSize: 20
-    }
-})
